8121a624a40c14ff0524e49ad9b7b0d6bab6690a
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-06 13:38:36 +0100

    Initial Commit

---
+++ Image display via URL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image display via URL
+//
+//  Created by Macbook Air 2017 on 6. 12. 2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
