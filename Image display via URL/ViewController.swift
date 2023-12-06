5ea2e3e809d0c730e68714f1f779e3a737fad960
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-06 13:41:46 +0100

    Implemented function for fetching image Data

--- Image display via URL/ViewController.swift
+++ Image display via URL/ViewController.swift
@@ -11,6 +11,18 @@ class ViewController: UIViewController {
     
     let PHOTO_URL_STRING = "https://homegrown-garden.com/cdn/shop/articles/how_to_take_care_of_bonsai_trees_header_4_1280x.png?v=1610622891"
     
+    private func fetchImageData() async -> Data? {
+        guard let url = URL(string: PHOTO_URL_STRING) else {
+            return nil
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            return data
+        }
+        catch {
+            return nil
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
